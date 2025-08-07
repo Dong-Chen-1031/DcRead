d88ea0e535fde773febbccead7c870d57270b77f
Dong-Chen-1031 <dcdcdc1031@gmail.com>
2025-08-07 14:57:24 +0800

    更新已讀測試功能，修正圖片 URL 生成邏輯，新增產生圖片 URL 的指令

--- cogs/read_cog.py
+++ cogs/read_cog.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 from discord import app_commands, Interaction
 import discord
 from utils.log import log
-
+from typing import Optional
+import random
 
 class ReadCog(commands.Cog):
     def __init__(self, bot):
@@ -24,13 +25,29 @@ class ReadCog(commands.Cog):
                 title="已讀測試",
                 color=discord.Color.green()
             )
-            embed.set_image(url=f"{settings.SERVER_URL}/img/{user.display_name}-{user.id}-{interaction.id}")
+            embed.set_image(url=f"{settings.SERVER_URL}/img/{user.display_name}-{user.id}-{random.randint(100000000, 1000000000)}")
             embed.set_footer(text=f"觸發者: {interaction.user.display_name}", icon_url=interaction.user.display_avatar.url)
             await user.send(embed=embed)
             await interaction.response.send_message(f"已發送訊息給 {user.mention}，請查看私訊！")
         except Exception as e:
             logging.error(f"發送訊息給 {user} 時發生錯誤: {e}")
             await interaction.response.send_message(f"發送訊息時發生錯誤: {e}", ephemeral=True)
+
+    @app_commands.command(name="url", description="產生已讀測試圖片 URL")
+    @app_commands.describe(target_user="對方名稱", user="通知接收者 (預設為自己)", 
+                            remark="備註")
+    async def url(self, interaction: Interaction, target_user: str, user: Optional[discord.User]=None, remark: Optional[str]=None):
+        """產生已讀測試圖片 URL"""
+        if user is None:
+            user = interaction.user
+        url = f"{settings.SERVER_URL}/img/{target_user}({remark})-{user.id}-{random.randint(100000000, 1000000000)}"
+        embed = discord.Embed(
+            title="已讀測試圖片 URL",
+            description=f"請將以下圖片傳給 `{target_user}`：\n```{url}```",
+            color=discord.Color.blue()
+        )
+
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(ReadCog(bot))
